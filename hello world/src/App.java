9e45576d6783cd83b1780a5dcd6dba7899940fee
Llongsf <2933854171@qq.com>
2023-09-02 15:47:57 +0800

    15:47

--- hello world/src/App.java
+++ hello world/src/App.java
@@ -12,5 +12,13 @@ public class App {
         System.out.println(o3 + "\n");
         System.out.println(x2 + x3 + x4);
         System.out.println("" + x2 + x3 + x4);
+
+        System.out.println(o.concat(o2));
+        second1(args);
+    }
+
+    public static void second1(String[] args) throws Exception {
+        System.out.println(Math.abs(-66.6));
+        System.out.println(Math.random());
     }
 }
