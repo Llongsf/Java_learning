9dc3896df23a0075f1c5839524c8a221ad290996
Llongsf <2933854171@qq.com>
2023-09-02 15:00:54 +0800

    ok

---
+++ hello world/src/App.java
@@ -0,0 +1,16 @@
+/* 
+package com.sun.naming
+*/
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x2 = 2;
+        int x3 = 3;
+        char x4 =99;
+        String o = "ooo";
+        String o2 = "eee";
+        String o3 = o + o2;
+        System.out.println(o3 + "\n");
+        System.out.println(x2 + x3 + x4);
+        System.out.println("" + x2 + x3 + x4);
+    }
+}
