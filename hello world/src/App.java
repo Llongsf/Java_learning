abc3293fc0c9500fc7a1193fa85c802bc20b0c0d
Llongsf <2933854171@qq.com>
2023-09-05 19:26:58 +0800

    9.5

--- hello world/src/App.java
+++ hello world/src/App.java
@@ -1,24 +1,21 @@
-/* 
-package com.sun.naming
-*/
+import java.io.*;
 public class App {
     public static void main(String[] args) throws Exception {
-        int x2 = 2;
-        int x3 = 3;
-        char x4 =99;
-        String o = "ooo";
-        String o2 = "eee";
-        String o3 = o + o2;
-        System.out.println(o3 + "\n");
-        System.out.println(x2 + x3 + x4);
-        System.out.println("" + x2 + x3 + x4);
-
-        System.out.println(o.concat(o2));
         second1(args);
     }
 
     public static void second1(String[] args) throws Exception {
         System.out.println(Math.abs(-66.6));
         System.out.println(Math.random());
+        int a = 5;
+        int b = 0;
+        try
+        {
+            System.out.println(a/b);
+        }
+        catch(ArithmeticException j)
+        {
+            j.printStackTrace();
+        }
     }
 }
