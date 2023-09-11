66285a66f12b2583880333616acb340d25581f36
Llongsf <2933854171@qq.com>
2023-09-11 20:45:34 +0800

    9/11

--- Static_b.java
+++ Static_b.java
@@ -17,7 +17,20 @@ public class Static_b
         int a = maxx();//静态方法可以直接调用，不需要实例化对象
         System.out.println(a);
         System.out.println(b);
+        Abbb abab = new Abbb();
+        abab.mm();
     }
     
 
+}
+abstract class  AbabClass extends Static_b//抽象类
+{
+    public abstract void mm();//抽象方法,自己不能有具体实现，其实现要由其子类进行定义提供
+}
+class Abbb extends AbabClass
+{
+    public void mm()//定义抽象方法
+    {
+        System.out.println("nihao");
+    }
 }
